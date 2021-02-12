13b18485866eb5c7fb9cea7c4339d9f93741c11c
Tian <tian.qiu142@gmail.com>
2021-02-11 21:57:34 -0800

    completed front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>A whole new app</div>
+  return (
+    <div>
+      <h1>Redux Counter</h1>
+      <h2>1</h2>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <div>
+        <input />
+      </div>
+      <button>Increment By Amount</button>
+      <button>Increment Async 1 sec</button>
+    </div>
+  )
 }
 
 export default App
